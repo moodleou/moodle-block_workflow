0d1ec24ca273f55b335ef22de60bf732ddc6e0ef
Tim Hunt <T.J.Hunt@open.ac.uk>
2016-08-02 16:57:44 +0100

    Bump version number for the 1.6 release.

--- version.php
+++ version.php
@@ -29,6 +29,6 @@ $plugin->requires  = 2015111600;
 $plugin->cron      = 60;
 $plugin->component = 'block_workflow';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = 'v1.5 for Moodle 3.0+';
+$plugin->release   = 'v1.6 for Moodle 3.0+';
 
 $plugin->outestssufficient = true;
